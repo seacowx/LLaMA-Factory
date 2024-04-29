ad2cc34e4555efc90b067474ae623734a19bd0ce
Hainiu Xu <seacow.xu@gmail.com>
2024-04-29 08:34:36 +0100

    add script

--- llama_ft.sh
+++ llama_ft.sh
@@ -1,18 +1,19 @@
-python src/train_bash.py \
+CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --stage sft \
     --do_train \
     --model_name_or_path /scratch/prj/inf_llmcache/hf_cache/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/1448453bdb895762499deb4176c1dd83b145fac1/ \
-    --dataset openpi_entity_attr_guided \
+    --dataset openpi_entity_guided \
     --template llama3 \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
-    --output_dir ../../../ft_weights/masktom/llama-8b/openpi_entity_attr/ \
+    --output_dir ../../../ft_weights/masktom/llama-8b/openpi_entity/ \
     --overwrite_cache \
+    --overwrite_output_dir \
     --per_device_train_batch_size 64 \
     --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
     --logging_steps 1 \
-    --save_steps 100 \
+    --save_steps 200 \
     --learning_rate 5e-5 \
     --num_train_epochs 3. \
     --plot_loss 
