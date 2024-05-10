8bee012b00c4d84fa3082a9e7b06a6369de9b5c5
Hainiu Xu <seacow.xu@gmail.com>
2024-05-10 13:39:50 +0100

    update dataset info, add ft scripts

---
+++ mixtral_ft.sh
@@ -0,0 +1,21 @@
+python src/train_bash.py \
+    --stage sft \
+    --do_train \
+    --model_name_or_path /scratch/prj/inf_llmcache/hf_cache/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/125c431e2ff41a156b9f9076f744d2f35dd6e67a/ \
+    --dataset openpi_entity_guided \
+    --template mistral \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --output_dir ../../../ft_weights/masktom/openpi_entity_guided_weights/ \
+    --overwrite_cache \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 1 \
+    --save_steps 200 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 2 \
+    --plot_loss \
+    --quantization_bit 4 \
+    --fp16 \
+    --upcast_layernorm
